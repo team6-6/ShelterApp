8bad807a581abaedcd59a242efb3085016063e68
sapirsa3 <45326895+sapirsa3@users.noreply.github.com>
2020-03-25 16:21:04 +0200

    DATABASE
    אחרי הוספת מחלקת בסיס נתונים עבור משתמשים חדשים שנרשמים לאפליקציה

--- MainActivity.java
+++ MainActivity.java
@@ -5,26 +5,70 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText usernameId,passwordId;
+    Button loginId,registerId;
+    DatabaseHelper db;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        db = new DatabaseHelper(this);
         setContentView(R.layout.activity_main);
-        findViewById(R.id.registerBtn).setOnClickListener(new View.OnClickListener(){
+        registerId=(Button)findViewById(R.id.registerBtn);
+        usernameId=(EditText)findViewById(R.id.username);
+        passwordId=(EditText)findViewById(R.id.password);
+        loginId=(Button)findViewById(R.id.loginBtn);
+
+        loginId.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                Toast.makeText(MainActivity.this,"Successfully registered !",Toast.LENGTH_LONG).show();
+                String user = usernameId.getText().toString().trim();
+                String pwd = passwordId.getText().toString().trim();
+                Boolean res = db.checkUser(user , pwd);
 
+                if (user.equals("") || pwd.equals("")){
+                    Toast.makeText(MainActivity.this, "One or more field are empty !", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    if (res == true) {
+                        Toast.makeText(MainActivity.this, "User dose'nt exist !", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Boolean passmatch = db.ReturnPasswordMatch(user,pwd);
+                        if(passmatch==true) {
+                            Toast.makeText(MainActivity.this, "Wrong password !", Toast.LENGTH_SHORT).show();
+                        }
+                        else{
+                         //   Toast.makeText(MainActivity.this, "good login !", Toast.LENGTH_SHORT).show();
+
+
+                            Intent first = new Intent(MainActivity.this, FirstActivity.class);
+                            startActivity(first);
+
+
+                        }
+
+                    }
+                }
             }
 
         });
+
+        registerId.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+
+                Intent intent=new Intent(MainActivity.this, RegisterActivity.class );
+                startActivity(intent);
+             //   Toast.makeText(MainActivity.this,"dfvdsggfv !",Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
     }
 
-    public void RegisterClick(View view) {
-        Intent intent=new Intent(this,RegisterActivity.class);
-        startActivity(intent);
-    }
+
 }
