a21cf6a8290ce1a79cc12ffd9fb926081a7b1aa8
karina1995 <44744114+karina1995@users.noreply.github.com>
2020-04-05 13:29:57 +0300

    הוספת activity של שינוי סיסמה,שכחתי ומפה

---
+++ src/main/java/com/example/shelter4u/BackgroundWorker.java
@@ -0,0 +1,75 @@
+package com.example.shelter4u;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.os.AsyncTask;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+
+public class BackgroundWorker extends AsyncTask<String,Void,String> {
+
+
+    Context context;
+    AlertDialog alertDialog;
+    BackgroundWorker (Context ctx){
+        context=ctx;
+    }
+    @Override
+    protected String doInBackground(String... param) {
+        String type = param[0];
+        String register_URL = "http://192.168.1.12/register.php";
+        if(type.equals("register")){
+            try {
+                String username = param[1];
+                String password = param[2];
+                URL url = new URL(register_URL);
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+                httpURLConnection.setRequestMethod("POST");
+                httpURLConnection.setDoOutput(true);
+                httpURLConnection.setDoInput(true);
+                OutputStream outputStream = httpURLConnection.getOutputStream();
+                BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream, "UTF-8"));
+                String post_data = URLEncoder.encode("username", "UTF-8") + "=" + URLEncoder.encode(username, "UTF-8") +
+                        "&" + URLEncoder.encode("password", "UTF-8") + "=" + URLEncoder.encode(password, "UTF-8");
+                bufferedWriter.write(post_data);
+                bufferedWriter.flush();
+                bufferedWriter.close();
+                outputStream.close();
+                InputStream inputStream = httpURLConnection.getInputStream();
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, "iso-8859-1"));
+                String result = "";
+                String line = " ";
+                while ((line = bufferedReader.readLine()) != null) {
+                    result += line;
+                }
+                bufferedReader.close();
+                inputStream.close();
+                httpURLConnection.disconnect();
+                return result;
+
+            }
+            catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+        return null;
+    }
+
+
+
+
+
+}
