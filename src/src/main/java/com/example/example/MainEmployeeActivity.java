88e7c7b365ccc950f1d634e7934696ba35255f5c
karina1995 <44744114+karina1995@users.noreply.github.com>
2020-04-06 20:48:17 +0300

    more activities

---
+++ src/src/main/java/com/example/example/MainEmployeeActivity.java
@@ -0,0 +1,103 @@
+package com.example.example;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainEmployeeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_employee);
+        ImageView list_shelters,list_sirens,map,requests,requests_admin,numbers,contant;
+        TextView change ;
+
+        list_shelters=(ImageView)findViewById(R.id.Shelterlist) ;
+        list_sirens= (ImageView)findViewById(R.id.Sirenlist);
+        map= (ImageView)findViewById(R.id.MapOption);
+        requests=(ImageView)findViewById(R.id.Requests);
+        requests_admin=(ImageView)findViewById(R.id.Requests_Admin);
+        numbers= (ImageView)findViewById(R.id.EmergencyNumber);
+        contant= (ImageView)findViewById(R.id.ContantUs);
+        change= (TextView) findViewById(R.id.changePass);
+        final String sessionId = getIntent().getStringExtra("EXTRA_SESSION_ID");
+
+        map.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent first = new Intent(MainEmployeeActivity.this, MapsActivity.class);
+                startActivity(first);
+            }
+        });
+
+        change.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent first = new Intent(MainEmployeeActivity.this, ChangeActivity.class);
+                first.putExtra("EXTRA_SESSION_ID2", sessionId);
+                startActivity(first);
+            }
+        });
+
+        list_shelters.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent first = new Intent(MainEmployeeActivity.this, SheltersActivity.class);
+                startActivity(first);
+            }
+        });
+
+        list_sirens.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent first = new Intent(MainEmployeeActivity.this, SirensActivity.class);
+                startActivity(first);
+            }
+        });
+
+        requests.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent first = new Intent(MainEmployeeActivity.this, RequestsActivity.class);
+                startActivity(first);
+            }
+        });
+
+        requests_admin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent first = new Intent(MainEmployeeActivity.this, RequestsAdminActivity.class);
+                startActivity(first);
+            }
+        });
+
+        numbers.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent first = new Intent(MainEmployeeActivity.this, NumbersActivity.class);
+                startActivity(first);
+            }
+        });
+
+        contant.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent first = new Intent(MainEmployeeActivity.this, ContactUsActivity.class);
+                startActivity(first);
+            }
+        });
+    }
+}
---
+++ src/src/main/java/com/example/example/RegisterActivity.java
@@ -0,0 +1,117 @@
+package com.example.example;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Pattern;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    public FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private static final String TAG = "RegisterActivity";
+    private int i=1;
+    EditText userId, passwordId, confirmId, q1, q2;
+    Button registerId;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        userId = (EditText) findViewById(R.id.username);
+        passwordId = (EditText) findViewById(R.id.password);
+        confirmId = (EditText) findViewById(R.id.confirm);
+        registerId = (Button) findViewById(R.id.registerBtn);
+        q1=(EditText) findViewById(R.id.confirm2);
+        q2=(EditText) findViewById(R.id.confirm3);
+        registerId.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addUser();
+            }
+        });
+
+    }
+
+    protected void addUser(){
+
+        final String user = userId.getText().toString().trim();
+        String pwd = passwordId.getText().toString().trim();
+        String cfrm = confirmId.getText().toString().trim();
+        String quetion1=q1.getText().toString().trim();
+        String quetion2=q2.getText().toString().trim();
+        Pattern UpperCasePatten = Pattern.compile("[A-Z ]");
+        Pattern lowerCasePatten = Pattern.compile("[a-z ]");
+        Pattern digitCasePatten = Pattern.compile("[0-9 ]");
+
+        if (user.equals("") || pwd.equals("")||quetion1.equals("")||quetion2.equals("")||cfrm.equals("")){
+            Toast.makeText(RegisterActivity.this, "Field are empty !", Toast.LENGTH_SHORT).show();
+        }
+        else if (user.length() <3 ) {
+            Toast.makeText(RegisterActivity.this, "user lenght must be atleast 3 character !!", Toast.LENGTH_SHORT).show();
+        }
+        else if (pwd.length() < 8 || pwd.length() > 12) {
+            Toast.makeText(RegisterActivity.this, "Password lenght must be 8-12 character !!", Toast.LENGTH_SHORT).show();
+        }
+        else if (!UpperCasePatten.matcher(pwd).find() && !lowerCasePatten.matcher(pwd).find() && !digitCasePatten.matcher(pwd).find()) {
+            Toast.makeText(RegisterActivity.this, "Password must contains only letters & digits !!", Toast.LENGTH_SHORT).show();
+
+        }
+        else if (!UpperCasePatten.matcher(pwd).find()) {
+            Toast.makeText(RegisterActivity.this, "Password must have atleast one upper character !!", Toast.LENGTH_SHORT).show();
+
+        }
+        else if (!lowerCasePatten.matcher(pwd).find()) {
+            Toast.makeText(RegisterActivity.this, "Password must have atleast one lower character !!", Toast.LENGTH_SHORT).show();
+
+        }
+        else if (!digitCasePatten.matcher(pwd).find()) {
+            Toast.makeText(RegisterActivity.this, "Password must have atleast one digit !!", Toast.LENGTH_SHORT).show();
+
+        }
+        else {
+            try {
+                DocumentReference noteref = db.collection("users").document(user);
+
+
+                // Create a new user with a first and last name
+                Map<String, Object> user_details = new HashMap<>();
+                user_details.put("password", pwd);
+                user_details.put("permission", "C");
+                user_details.put("Father", quetion1);
+                user_details.put("School", quetion2);
+
+// Add a new document with a generated ID
+                db.collection("users").document(user).set(user_details)
+                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void documentReference) {
+                                Log.d(TAG, "DocumentSnapshot added with ID: " + user);
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Log.w(TAG, "Error adding document", e);
+                            }
+                        });
+            } catch (Exception e) {
+                Toast.makeText(RegisterActivity.this, "already exist!", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+    }
+}
