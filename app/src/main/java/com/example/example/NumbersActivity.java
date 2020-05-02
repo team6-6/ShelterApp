b96c58cd174f01770ad7fa192387f6b07e669b12
sapirsa3 <sapirsa3@ac.sce.ac.il>
2020-05-02 15:17:33 +0300

    PM2020T6-23

--- app/src/main/java/com/example/example/NumbersActivity.java
+++ app/src/main/java/com/example/example/NumbersActivity.java
@@ -1,14 +1,79 @@
 package com.example.example;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
+
+    private static final String TAG = "NumbersActivity";
+    private ArrayAdapter<String> adpter;
+    private ListView listView;
+    private TextView backText;
+    private FirebaseFirestore db=FirebaseFirestore.getInstance();
+    private ArrayList<String> arrayList=new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+        listView=(ListView) findViewById(R.id.listEmergency);
+        backText=(TextView) findViewById(R.id.backEmergency);
+        final CollectionReference collectionReference = db.collection("Emergency");
+        //Calling the get() method with a callback function
+        adpter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,arrayList);
+        listView.setAdapter(adpter);
+
+        collectionReference.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    //Task is successful
+                    //Running enhanced for loop to get each document
+                    String text1="\nOrganization                     Number";
+                    arrayList.add(text1);
+                    for (QueryDocumentSnapshot documentSnapshot: task.getResult()){
+                        String id=documentSnapshot.getId();
+                        String i=documentSnapshot.get("number").toString();
+                        String text=id+"                     "+i;
+                        arrayList.add(text);
+                        adpter.notifyDataSetChanged();
+
+                        //Printing data of each document to log
+                        Log.i(TAG, "onComplete: query data: " + documentSnapshot.getData());
+                    }
+                }else{
+                    //Task was not successful
+                    Log.e(TAG, "onComplete: ERROR: " + task.getException().getLocalizedMessage() );
+                }
+            }
+        });
+
+        backText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent first = new Intent(NumbersActivity.this, MainCivilianActivity.class);
+                startActivity(first);
+
+            }
+        });
+
     }
 }
