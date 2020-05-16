e386a11b7f66b26ddcd698dfc5484920c1055956
yaelbu11 <45327181+yaelbu11@users.noreply.github.com>
2020-05-16 15:26:34 +0300

    Update NumbersActivityTestIntegration.java

--- app/src/test/java/com/example/example/NumbersActivityTestIntegration.java
+++ app/src/test/java/com/example/example/NumbersActivityTestIntegration.java
@@ -8,7 +8,7 @@ import org.robolectric.annotation.Config;
 
 import static org.junit.Assert.*;
 
-@Config(sdk = 28)
+@Config(sdk = 21)
 @RunWith(RobolectricTestRunner.class)
 public class NumbersActivityTestIntegration {
 
