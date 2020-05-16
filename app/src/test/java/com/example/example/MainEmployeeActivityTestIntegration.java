26f2d902c7bd287004e9ea0a68cd549daea843c6
karina1995 <karindu@ac.sce.ac.il>
2020-05-17 01:31:15 +0300

    PM2020T6-148

---
+++ app/src/test/java/com/example/example/MainEmployeeActivityTestIntegration.java
@@ -0,0 +1,36 @@
+package com.example.example;
+
+import android.content.Intent;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
+import static org.junit.Assert.*;
+
+@Config(sdk = 21, packageName="com.example.example")
+@RunWith(RobolectricGradleTestRunner.class)
+public class MainEmployeeActivityTestIntegration {
+
+    @Test
+    public void SendWhatYouGetAdminTest(){
+        MainAdminActivity activity= Robolectric.buildActivity(MainAdminActivity.class).create().get();
+        User use=new User();
+        use.setName("dana13");
+        use.setPassword("Dana1234");
+        use.setPermission("B");
+        activity.getIntent().putExtra("EXTRA_SESSION_ID",use.getName());
+        activity.setSessionId();
+        Intent intent=activity.ToChangePassword();
+        assertEquals(intent.getExtras().getString("EXTRA_SESSION_ID2"),use.getName());
+    }
+
+    @Test
+    public void LogoutAccount(){
+        MainAdminActivity activity= Robolectric.buildActivity(MainAdminActivity.class).create().get();
+        assertTrue(activity.Logout());
+    }
+
+}
