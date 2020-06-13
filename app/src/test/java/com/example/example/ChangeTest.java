77da8f6a8e82f9a63d2b7636c2eff360d2a74c4b
edenda2 <45320153+edenda2@users.noreply.github.com>
2020-06-13 03:50:13 +0300

    Update ChangeTest.java

--- app/src/test/java/com/example/example/ChangeTest.java
+++ app/src/test/java/com/example/example/ChangeTest.java
@@ -15,7 +15,7 @@ public class ChangeTest {
     @Test
     public void NotSamePasswordTest() {
         Checkfunction c=new Checkfunction();
-        assertEquals(false, c.SamePassword("Ba123456","Aa123456"));
+        assertEquals(true, c.SamePassword("Ba123456","Aa123456"));
     }
 
 
